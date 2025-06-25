0b6946a1b11d0410b8cefb8961fa758989760466
Ellyan <EllyanG.Lopes@gmail.com>
2025-06-25 13:02:58 -0300

    feat(login): add CPF mask directive

--- frontend/src/app/pages/login/login.component.spec.ts
+++ frontend/src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { CpfMaskDirective } from '../../directives/cpf-mask.directive';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +9,7 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent, CpfMaskDirective]
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
