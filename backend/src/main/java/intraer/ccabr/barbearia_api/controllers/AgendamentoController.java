74dc93ec47ff65f8acfe82d2672acf77e8e99b40
Ellyan <EllyanG.Lopes@gmail.com>
2025-07-01 16:26:18 -0300

    Allow late appointment cancellations

--- backend/src/main/java/intraer/ccabr/barbearia_api/controllers/AgendamentoController.java
+++ backend/src/main/java/intraer/ccabr/barbearia_api/controllers/AgendamentoController.java
@@ -265,14 +265,6 @@ public class AgendamentoController {
                         .body("Não é possível desmarcar um agendamento que já ocorreu.");
             }
 
-            LocalDateTime agora = LocalDateTime.now().withSecond(0).withNano(0);
-            LocalDateTime dataHoraAgendamento = LocalDateTime.of(
-                    agendamento.getData(), agendamento.getHora())
-                    .withSecond(0).withNano(0);
-            if (dataHoraAgendamento.minusMinutes(15).isBefore(agora)) {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                        .body("Não é possível desmarcar com menos de 15 minutos de antecedência.");
-            }
 
             try {
                 agendamentoService.delete(id);
