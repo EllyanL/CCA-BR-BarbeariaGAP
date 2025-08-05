94fe16b34f42af2d89de3689d727efccb89e35f4
Ellyan <EllyanG.Lopes@gmail.com>
2025-08-05 09:29:41 -0300

    feat: add scheduling configuration management

---
+++ frontend/src/app/services/configuracoes-agendamento.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface ConfiguracaoAgendamento {
+  horarioInicio: string;
+  horarioFim: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfiguracoesAgendamentoService {
+  private readonly apiUrl = `${environment.apiUrl}/configuracoes/agendamento`;
+
+  constructor(private http: HttpClient) {}
+
+  getConfig(): Observable<ConfiguracaoAgendamento> {
+    return this.http.get<ConfiguracaoAgendamento>(this.apiUrl);
+  }
+
+  updateConfig(config: ConfiguracaoAgendamento): Observable<ConfiguracaoAgendamento> {
+    return this.http.put<ConfiguracaoAgendamento>(this.apiUrl, config);
+  }
+}
+
