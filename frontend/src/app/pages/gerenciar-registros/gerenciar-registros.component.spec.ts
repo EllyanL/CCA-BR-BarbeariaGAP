76c83120b801fad242744d445f80eb34ae088680
Ellyan <EllyanG.Lopes@gmail.com>
2025-08-08 08:12:05 -0300

    test: ensure schedule fetch requires dates

---
+++ frontend/src/app/pages/gerenciar-registros/gerenciar-registros.component.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+
+import { GerenciarRegistrosComponent } from './gerenciar-registros.component';
+import { AgendamentoService } from '../../services/agendamento.service';
+import { LoggingService } from '../../services/logging.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+describe('GerenciarRegistrosComponent', () => {
+  let component: GerenciarRegistrosComponent;
+  let fixture: ComponentFixture<GerenciarRegistrosComponent>;
+  let agendamentoService: jasmine.SpyObj<AgendamentoService>;
+  let snackBar: jasmine.SpyObj<MatSnackBar>;
+
+  beforeEach(() => {
+    agendamentoService = jasmine.createSpyObj('AgendamentoService', ['listarFiltrado']);
+    agendamentoService.listarFiltrado.and.returnValue(of([]));
+    snackBar = jasmine.createSpyObj('MatSnackBar', ['open']);
+
+    TestBed.configureTestingModule({
+      declarations: [GerenciarRegistrosComponent],
+      providers: [
+        { provide: AgendamentoService, useValue: agendamentoService },
+        { provide: LoggingService, useValue: { error: () => {} } },
+        { provide: MatSnackBar, useValue: snackBar }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+
+    fixture = TestBed.createComponent(GerenciarRegistrosComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('não chama o serviço quando datas estão ausentes', () => {
+    component.dataInicial = undefined;
+    component.dataFinal = new Date();
+
+    component.carregarAgendamentos();
+
+    expect(agendamentoService.listarFiltrado).not.toHaveBeenCalled();
+    expect(snackBar.open).toHaveBeenCalled();
+  });
+});
+
