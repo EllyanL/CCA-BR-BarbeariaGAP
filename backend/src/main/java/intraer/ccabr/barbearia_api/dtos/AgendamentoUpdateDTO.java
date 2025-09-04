733a4b7c20118743ed93c14806cf915644117f66
Ellyan <EllyanG.Lopes@gmail.com>
2025-09-04 15:43:20 -0300

    Ajusta formato de data para dd/MM/yyyy

--- backend/src/main/java/intraer/ccabr/barbearia_api/dtos/AgendamentoUpdateDTO.java
+++ backend/src/main/java/intraer/ccabr/barbearia_api/dtos/AgendamentoUpdateDTO.java
@@ -3,10 +3,13 @@ package intraer.ccabr.barbearia_api.dtos;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class AgendamentoUpdateDTO {
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate data;
     private LocalTime hora;
     private String diaSemana;
