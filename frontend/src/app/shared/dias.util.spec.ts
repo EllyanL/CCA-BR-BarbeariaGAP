2abbdc99b7e2ecac95744adb370749e02c1ff83f
EllyanL <ellyang.Lopes@gmail.com>
2025-09-23 09:39:46 -0300

    remove tests

--- frontend/src/app/shared/dias.util.spec.ts
+++
@@ -1,9 +0,0 @@
-import { normalizeDia } from './dias.util';
-
-describe('normalizeDia', () => {
-  it('normalizes accented weekday names', () => {
-    expect(normalizeDia('terça')).toBe('terca');
-    expect(normalizeDia('TERÇA')).toBe('terca');
-    expect(normalizeDia('Quinta')).toBe('quinta');
-  });
-});
