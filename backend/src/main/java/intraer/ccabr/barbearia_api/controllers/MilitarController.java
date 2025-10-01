26ffbae251f40143aa7d9b85a1c4a236052cec69
Ellyan <EllyanG.Lopes@gmail.com>
2025-10-01 09:41:34 -0300

    Restrict militar endpoints to admin

--- backend/src/main/java/intraer/ccabr/barbearia_api/controllers/MilitarController.java
+++ backend/src/main/java/intraer/ccabr/barbearia_api/controllers/MilitarController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import intraer.ccabr.barbearia_api.services.MilitarService;
 
 @RestController
 @RequestMapping("/api/militares")
+@PreAuthorize("hasRole('ADMIN')")
 public class MilitarController {
     private final MilitarService militarService;
     private static final Logger logger = LoggerFactory.getLogger(MilitarController.class);
